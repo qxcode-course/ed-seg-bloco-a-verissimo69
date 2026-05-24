f4c80684d21088062ce63b801889a4d012a793d8
Joao Victor Verissimo <joaovictorverissimo20@gmail.com>
2026-05-24 20:08:52 -0300

    lista_3

--- ed/lista_d3/src/go/main.go
+++ ed/lista_d3/src/go/main.go
@@ -43,7 +43,68 @@ func (l *LList) insertBefore(mark *Node, value int) {
 	mark.prev.next = n
 	mark.prev = n
 }
+func equals(list1, list2 *LList) bool {
+	if list1.size != list1.size {
+		return false
+	}
+	for i1, i2 := list1.root.next, list2.root.next; i1 != list1.root || i2 != list2.root; i1, i2 = i1.next, i2.next {
+		if i1.Value != i2.Value {
+			return false
+		}
+	}
+	return true
+}
 
+func addsorted(list *LList, value int) {
+	curr := list.root.next
+	for curr != list.root && curr.Value < value {
+		curr = curr.next
+	}
+
+	newNode := &Node{
+		Value: value,
+		next:  curr,
+		prev:  curr.prev,
+		root:  curr.root,
+	}
+
+	curr.prev.next = newNode
+	curr.prev = newNode
+}
+func merge(list1, list2 *LList) *LList {
+	result := NewLList()
+	// Iniciamos a recursão apenas com o primeiro nó real de cada lista
+	mergeRec(list1.root.next, list2.root.next, result)
+	return result
+}
+
+func mergeRec(n1, n2 *Node, result *LList) {
+	// 1. Condição de parada: ambas as listas chegaram no sentinela
+	if n1 == n1.root && n2 == n2.root {
+		return
+	}
+
+	// 2. Escolhemos pegar o valor do n2 se:
+	// - A lista 1 já acabou (n1 == n1.root) OU
+	// - A lista 2 não acabou E o valor de n2 é menor que n1
+	if n1 == n1.root || (n2 != n2.root && n2.Value < n1.Value) {
+		result.PushBack(n2.Value)
+		mergeRec(n1, n2.next, result) // Avança apenas n2
+	} else {
+		// 3. Caso contrário, com certeza devemos pegar o n1
+		result.PushBack(n1.Value)
+		mergeRec(n1.next, n2, result) // Avança apenas n1
+	}
+}
+
+func (l *LList) reverse() []int {
+	var ele []int
+	for i := l.root.prev; i != l.root; i = i.prev {
+		ele = append(ele, i.Value)
+	}
+	return ele
+
+}
 
 func str2list(serial string) *LList {
 	serial = serial[1 : len(serial)-1]
@@ -56,6 +117,31 @@ func str2list(serial string) *LList {
 		ll.PushBack(value)
 	}
 	return ll
+}
+func Stringg(list []int) string {
+	var ele string
+	ele += "["
+	for i := 0; i < len(list); i++ {
+		ele += strconv.Itoa(list[i])
+		if i < len(list)-1 {
+			ele += ", "
+		}
+	}
+
+	ele += "]"
+	return ele
+}
+
+func String(l *LList) string {
+	var elements []string
+
+	// Percorre a lista ignorando o nó sentinela (root)
+	for curr := l.root.next; curr != l.root; curr = curr.next {
+		elements = append(elements, strconv.Itoa(curr.Value))
+	}
+
+	// Formata a saída unindo os elementos com vírgula e espaço, cercados por colchetes
+	return "[" + strings.Join(elements, ", ") + "]"
 }
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -77,29 +163,29 @@ func main() {
 
 		switch cmd {
 		case "compare":
-			// lla := str2list(args[1])
-			// llb := str2list(args[2])
-			// if equals(lla, llb) {
-			// 	fmt.Println("iguais")
-			// } else {
-			// 	fmt.Println("diferentes")
-			// }
+			lla := str2list(args[1])
+			llb := str2list(args[2])
+			if equals(lla, llb) {
+				fmt.Println("iguais")
+			} else {
+				fmt.Println("diferentes")
+			}
 		case "addsorted":
-			// lla := NewLList()
-			// for i := 1; i < len(args); i++ {
-			// 	value, _ := strconv.Atoi(args[i])
-			// 	addsorted(lla, value)
-			// }
-			// fmt.Println(lla)
+			lla := NewLList()
+			for i := 1; i < len(args); i++ {
+				value, _ := strconv.Atoi(args[i])
+				addsorted(lla, value)
+			}
+			fmt.Println(String(lla))
 		case "reverse":
-			// lla := str2list(args[1])
-			// reverse(lla)
-			// fmt.Println(lla)
+			lla := str2list(args[1])
+			list := lla.reverse()
+			fmt.Println(Stringg(list))
 		case "merge":
-			// lla := str2list(args[1])
-			// llb := str2list(args[2])
-			// merged := merge(lla, llb)
-			// fmt.Println(merged)
+			lla := str2list(args[1])
+			llb := str2list(args[2])
+			merged := merge(lla, llb)
+			fmt.Println(String(merged))
 		case "end":
 			return
 		default:
