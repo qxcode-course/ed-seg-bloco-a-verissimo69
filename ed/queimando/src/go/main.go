22ead1ec097d351d0cf4286f906ca15022bf4b76
Joao Victor Verissimo <joaovictorverissimo20@gmail.com>
2026-06-09 10:38:27 -0300

    Queimando

--- ed/queimando/src/go/main.go
+++ ed/queimando/src/go/main.go
@@ -6,9 +6,44 @@ import (
 	"os"
 )
 
-func burnTrees(grid [][]rune, l, c int) {
-	stack := NewStack[Pos]()
-	_ , _ , _ = mat, l, c
+type Pos struct {
+	l, c int
+}
+
+func burnTrees(grid [][]rune, startL, startc int) {
+	stack := []Pos{}
+	stack = append(stack, Pos{l: startL, c: startc})
+
+	dr := []int{-1, 1, 0, 0}
+	dc := []int{0, 0, -1, 1}
+
+	nl := len(grid)
+	if nl == 0 {
+		return
+	}
+
+	nc := len(grid[0])
+	for len(stack) > 0 {
+
+		topIndex := len(stack) - 1
+		curr := stack[topIndex]
+		stack = stack[:topIndex]
+
+		if curr.l < 0 || curr.l >= nl || curr.c < 0 || curr.c >= nc {
+			continue
+		}
+
+		if grid[curr.l][curr.c] == '#' {
+			grid[curr.l][curr.c] = 'o'
+			for i := 0; i < 4; i++ {
+				nextL := curr.l + dr[i]
+				nextC := curr.c + dc[i]
+				if nextL >= 0 && nextL < nl && nextC >= 0 && nextC < nc && grid[nextL][nextC] == '#' {
+					stack = append(stack, Pos{l: nextL, c: nextC})
+				}
+			}
+		}
+	}
 
 	// Essa função deve usar uma list como pilha
 	// e marcar as árvores na matriz como queimados
